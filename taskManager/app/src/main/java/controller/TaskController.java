bea4224432d709122a98a19204c2d94cb70a4b84
Jefferson Cardoso <maestro.jfc@gmail.com>
2023-08-12 13:13:01 -0300

    Implantação do método Save().

--- taskManager/app/src/main/java/controller/TaskController.java
+++ taskManager/app/src/main/java/controller/TaskController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
@@ -14,7 +15,39 @@ import util.ConnectionFactory;
 public class TaskController {
     
    public void save(Task task){
-        
+        String sql = "INSERT INTO tasks ("
+                + "idProject,"
+                + "name,"
+                + "description,"
+                + "completed,"
+                + "observation,"
+                + "deadline,"
+                + "createdAt,"
+                + " updatedAt)"
+                + "VALUES (?,?,?,?,?,?,?,?)";
+       
+       Connection conn = null;
+       // obj que auxilia na preparação do comando SQL que será executada no BD.
+       PreparedStatement statement = null;
+       
+       try{
+           conn = ConnectionFactory.getConnection();
+           statement = conn.prepareStatement(sql);
+           //setando os parâmetros
+           statement.setInt(1,task.getIdProject());
+           statement.setString(2, task.getName());
+           statement.setString(3, task.getDescription());
+           statement.setBoolean(4, task.getCompleted());
+           statement.setString(5, task.getObservation());
+           statement.setDate(6,new Date(task.getDeadline().getTime()));
+           statement.setDate(7,new Date(task.getCreatedAt().getTime()));
+           statement.setDate(8,new Date(task.getUpdatedAt().getTime()));
+           statement.execute();           
+       }catch(SQLException ex){
+           throw new RuntimeException("Erro ao incluir a tarefa!" + ex.getMessage(),ex);           
+       }finally{
+           ConnectionFactory.closeConnection(conn);
+       }        
    }
     
    public void update(Task task){
