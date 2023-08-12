b716148d06ad6c716821dd24e6ab25fe8a5e55c3
Jefferson Cardoso <maestro.jfc@gmail.com>
2023-08-11 21:17:01 -0300

    Criação da classe 'ConnectionFactory' para o acesso ao banco de dados

--- taskManager/app/src/main/java/taskManager/App.java
+++ taskManager/app/src/main/java/taskManager/App.java
@@ -3,12 +3,15 @@
  */
 package taskManager;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+import java.sql.Connection;
+import util.ConnectionFactory;
 
+public class App {
+    
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        
+        Connection c = ConnectionFactory.getConnection();
+        
+        ConnectionFactory.closeConnection(c);        
     }
 }
--- taskManager/app/src/main/java/util/ConnectionFactory.java
+++ taskManager/app/src/main/java/util/ConnectionFactory.java
@@ -1,10 +1,12 @@
 
 package util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 /**
  * Classe Responsável por criar a conexão com o banco de dados, e devolver 
  * para onde for necessário realizar consultas e manipulação do BD.
- * @author Jefferson
+ * @author Jefferson Cardoso
  */
 public class ConnectionFactory {
     
@@ -13,4 +15,23 @@ public class ConnectionFactory {
     public static final String USER = "root"; 
     public static final String PASS = ""; 
     
+    public static Connection getConnection(){
+        try{
+            //carrega o drive a ser usado pela classe
+            Class.forName(DRIVER);
+            return DriverManager.getConnection(URL,USER,PASS);
+        }catch (Exception ex){
+            throw new RuntimeException ("Erro na conexão com o Banco de Dados",ex);            
+        }
+    }
+    
+    public static void closeConnection(Connection connection){
+        try{
+            if(connection != null){
+                connection.close();
+            }
+        }catch (Exception ex){
+            throw new RuntimeException("Erro ao fechar a conexão com o Banco de Dados",ex);
+        }
+    }
 }
